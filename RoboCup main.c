49fea837fa2f3dc8c232c7fbe21d4d07a35cf607
peterkrull <31969909+peterkrull@users.noreply.github.com>
2019-09-10 14:41:15 +0200

    Update RoboCup main.c

--- RoboCup main.c
+++ RoboCup main.c
@@ -1,8 +1,7 @@
-
-// Kode til konfigurering af I/O til Minstorms EV3
 #pragma config(Sensor, S1, ultrasonic, sensorEV3_Ultrasonic)
 #pragma config(Sensor, S2, touchsens, sensorEV3_Touch)
 #pragma config(Sensor, S3, colorsens, sensorEV3_Color)
+#pragma config(Sensor, S4, homesens, sensorEV3_Touch)
 #pragma config(Motor, motorA, motorR, tmotorEV3_Large, PIDControl, encoder)
 #pragma config(Motor, motorB, motorL, tmotorEV3_Large, PIDControl, encoder)
 #pragma config(Motor, motorC, grapmotor, tmotorEV3_Medium, PIDControl, encoder)
@@ -18,42 +17,31 @@
 #define task8 true  // Kør rundt om flasken (gentagelse af task6)
 #define task9 true  // Kør ind til midten af målstregen
 
-bool changetask = false;  	// bruges til at definere hvornår robotten er imellem to opgaver
-bool linetrack = true;		// Bruges til at skifte mellem linetracking og frikørsel
-bool racedone = false;		// Sættes automatisk til sandt når task9 er gennemført
-bool celebration = false; 	// Sættes til true sammen med racedone, og sættes til false for at afslutte hele programmet.
-int curr_task = 0;		  	// For at teste specifikke udfordringer, skift dette tal
-int dir = 1;			  	// sæt 1 for at køre på venstre side af grå streg, 0 for at køre på højre
-float perfect_line;		  	// variabel til at holde information om den kalibrerede linje
-float speed = 10; 			// Robottens hastighed i PID-loopet. 
-
-// PID værdier
-float Kp = 0.15;
-float Ki = 0.000001;
-float Kd = 0.028;
+bool changetask = false;  // bruges til at definere hvornår robotten er imellem to opgaver
+bool linetrack = true;	// Bruges til at skifte mellem linetracking og frikørsel
+bool racedone = false;	// Sættes automatisk til sandt når task9 er gennemført
+bool celebration = false; // Sættes til true sammen med racedone, og sættes til false for at afslutte hele programmet.
+int curr_task = 0;		  // For at teste specifikke udfordringer, skift dette tal
+int dir = 1;			  // sæt 1 for at køre på venstre side af grå streg, 0 for at køre på højre
+int black_counter;		  // Bruges til at holde styr på antallet af krydsede sorte linjer
+float perfect_line;		  // variabel til at holde information om den kalibrerede linje
+float speed = 25;		  // Robottens hastighed i PID-loopet.
 
 // Variabel til at holde sensor aflæsning
 int line_sensor_val;
 
-// Konstante variabler til lysværdierne af stregerne
-const float white_val = 64;
-// bane 64 - papir 73
-const float gray_val = 27;
-// bane 38 - papir 40
-const float black_val = 5;
-//bane 5 - papir 7
-const float speed = 20;
-
+const float white_val = 73; // Variabel til værdien af den hvide del af banen
+const float gray_val = 40;  // Variabel til værdien af den grå del af banen
+const float black_val = 5;  // Variabel til værdien af den sorte del af banen
 
 task Linefollow_PID();
+//task grap_homing();
+//task color_calibrate();
 task main()
 {
-	clearTimer(T1);
-	perfect_line = gray_val + ((white_val - gray_val) / 2); // Udregner værdien af den perfekte grå/hvide linje ud fra de forudbestemte værdier
-
+	perfect_line = gray_val + ((white_val - gray_val) / 2);
 	while (racedone == false) // Main loop til at køre når race endnu ikke er færdig
 	{
-		int colorsens;
 		line_sensor_val = SensorValue(colorsens); // Værdien der læses fra farvesensoren. Skal næsten altid bruges, og er derfor i starten af vores loop.
 
 		//Line tacking function, kan slås til/fra ved at sætte linetrack = true; / linetrack = false;
@@ -62,101 +50,104 @@ task main()
 			startTask(Linefollow_PID);
 		}
 
-		/* Selv-diskvalificerende
-		short disctime = 30000;
-		if (timer1 > disctime)
+		if (curr_task == 1)
 		{
-			racedone = true;
-		}
-		*/
-	
-		// Betingelser for udførelse af opgave 1
-		if (task1 == true && curr_task == 1)
-		{
-			// Indsæt opgave 1 loop her **********************
-		}
-
-		// Betingelser for udførelse af opgave 2
-		else if (task2 == true && curr_task == 2 || task1 == false && curr_task == 1 && task2 == true)
-		{
-			if (curr_task == 1)
+			if (task1 == true) // Betingelser for udførelse af opgave 1
 			{
-				curr_task = 2;
+				// Indsæt opgave 1 loop her **********************
 			}
-
-			// Indsæt opgave 2 loop her **********************
+			else
+			{
+				curr_task++;
+				black_counter = black_counter++;
+			}
 		}
 
-		// Betingelser for udførelse af opgave 3
-		else if (task3 == true && curr_task == 3 || task2 == false && curr_task == 2 && task3 == true)
+		if (curr_task == 2)
 		{
-			if (curr_task == 2)
+			if (task2 == true) // Betingelser for udførelse af opgave 2
 			{
-				curr_task = 3;
+				// Indsæt opgave 2 loop her **********************
 			}
-
-			// Indsæt opgave 3 loop her **********************
+			else
+			{
+				curr_task++;
+				black_counter = black_counter++;
+			}
 		}
 
-		// Betingelser for udførelse af opgave 4
-		else if (task4 == true && curr_task == 4 || task3 == false && curr_task == 3 && task4 == true)
+		if (curr_task == 3)
 		{
-			if (curr_task == 3)
+			if (task3 == true) // Betingelser for udførelse af opgave 3
 			{
-				curr_task = 4;
+				// Indsæt opgave 3 loop her **********************
 			}
-
-			// Indsæt opgave 4 loop her **********************
+			else
+			{
+				curr_task++;
+				black_counter = black_counter++;
+			}
 		}
 
-		// Betingelser for udførelse af opgave 5
-		else if (task5 == true && curr_task == 5 || task4 == false && curr_task == 4 && task5 == true)
+		if (curr_task == 4)
 		{
-			if (curr_task == 4)
+			if (task4 == true) // Betingelser for udførelse af opgave 4
 			{
-				curr_task = 5;
+				// Indsæt opgave 4 loop her **********************
 			}
-
-			// Indsæt opgave 5 loop her **********************
+			else
+			{
+				curr_task++;
+			}
 		}
 
-		// Betingelser for udførelse af opgave 6 og 8 (de er ens)
-		else if (task6 == true && curr_task == 6 || task5 == false && curr_task == 5 && task6 == true	 // opgave 6
-				 || task8 == true && curr_task == 8 || task7 == false && curr_task == 7 && task8 == true) // opgave 8
+		if (curr_task == 5)
 		{
-			if (curr_task == 6)
+			if (task5 == true) // Betingelser for udførelse af opgave 5
 			{
-				curr_task = 7;
+				// Indsæt opgave 5 loop her **********************
 			}
-
-			else if (curr_task == 8)
+			else
 			{
-				curr_task = 9;
+				curr_task++;
+				black_counter = black_counter++;
 			}
-
-			// Indsæt opgave 6 og 8 loop her **********************
 		}
 
-		// Betingelser for udførelse af opgave 7
-		else if (task7 == true && curr_task == 7 || task6 == false && curr_task == 6 && task7 == true)
+		if (curr_task == 6 || curr_task == 8) // Det her vil ikke virke. Skal nok få det ordnet.
 		{
-			if (curr_task == 6)
+			if (task6 == true || task8 == true) // Betingelser for udførelse af opgave 6 og 8
 			{
-				curr_task = 7;
+				// Indsæt opgave 6 og 8 loop her **********************
 			}
-
-			// Indsæt opgave 7 loop her **********************
+			else
+			{
+				curr_task++;
+			}
 		}
 
-		// Betingelser for udførelse af opgave 9
-		else if (task9 == true && curr_task == 9 || task8 == false && curr_task == 8 && task9 == true)
+		if (curr_task == 7)
 		{
-			if (curr_task == 8)
+			if (task7 == true) // Betingelser for udførelse af opgave 7
 			{
-				curr_task = 9;
+				// Indsæt opgave 7 loop her **********************
 			}
+			else
+			{
+				curr_task++;
+			}
+		}
 
-			// Indsæt opgave 9 loop her **********************
+		if (curr_task == 9)
+		{
+			if (task9 == true) // Betingelser for udførelse af opgave 9
+			{
+				// Indsæt opgave 9 loop her **********************
+			}
+			else
+			{
+				curr_task++;
+			}
 		}
 	}
 
@@ -164,17 +155,21 @@ task main()
 	{
 		// Afspil lyd
 		// Kør rundt i cirkler
-		// �?ben og luk grappen
+		// Åben og luk grappen
 	}
 }
-
 task Linefollow_PID() // Funktionen der bruges til at følge linjen ved brug af PID-loop
 {
+
+	// PID konstanter
+	float Kp = 0.15;
+	float Ki = 0.000001;
+	float Kd = 0.028;
+
 	// variabler brugt til PID
-	// PID konstanter findes i toppen
 	float deltaErr = 0;
 	float turn = 0;
-	float errors;
+	float errors = 0;
 	float error_sum = 0;
 	float prev_error = 0;
 	int multi;
@@ -192,6 +187,12 @@ task Linefollow_PID() // Funktionen der bruges til at følge linjen ved brug af 
 	// Udregn størrelsen af fejlen : Proportionel
 	errors = multi * (line_sensor_val - perfect_line);
 
+	int color_diff = white_val - gray_val / 2;
+	if (errors > color_diff)
+		errors = color_diff;
+	else if (errors > -color_diff)
+		errors = -color_diff;
+
 	// Udregn sum af fejl : Integral
 	error_sum += errors;
 
@@ -208,59 +209,65 @@ task Linefollow_PID() // Funktionen der bruges til at følge linjen ved brug af 
 	setMotorSpeed(motorR, speed - ((turn * speed) / 10));
 	setMotorSpeed(motorL, speed + ((turn * speed) / 10));
 }
-
-/*UDATERET KODE, KAN MÅSKE BRUGES SENERE
-  Var det oprindelige forsøg på at identificere de forskellige farver og handle på dem.
-
-// Sæt initial states for R og L
-int stateR = 1;
-int stateL = 0;
-int stateonce = 0;
-if (gray_val - gray_black_var <= line_sensor_val <= gray_val + white_gray_var) // is gray
+/*
+task grap_homing()
 {
-	setLEDColor(ledGreen); // Sæt LED farve til grøn, når robotten er på en grå linje
-	colorread = 1;
-	for (stateonce = 0; stateonce < 1; stateonce++)
+}*/
+
+task color_calibrate() // Funktion til kalibrering af farvesensor
+{
+	bool color_cal;
+	if (SensorValue(touchsens) == 1) // Hvis knappen er trykket ned i mere end 2 sekunder igangsættes farvekalibrering
 	{
-		if (stateR > stateL)
+		clearTimer(T2); // reset af timer2
+		while (SensorValue(touchsens) == 1)
 		{
-			stateR = 0;
-			stateL = 1;
-		}
-		else if (stateR < stateL)
-		{
-			stateR = 1;
-			stateL = 0;
+			setLEDColor(ledOrangeFlash);
+			if (time1[T2] > 2000) // hvis timer 2 tælller over 2000 ms
+			{
+				color_cal = true; // igangsættes kalibrering
+				setLEDColor(ledGreenFlash);
+			}
 		}
 	}
-	setMotorSpeed(motorR, 10);
-	setMotorSpeed(motorL, 10);
-}
-
-else if (0 < line_sensor_val < gray_val - gray_black_var) // is black
-{
-	setLEDColor(ledOrange); // Sæt LED farve til orange, når robotten er på en sort linje
-	colorread = 2;
-	setMotorSpeed(motorR, 0);
-	setMotorSpeed(motorL, 0);
-}
-
-else if (gray_val + white_gray_var < line_sensor_val < 100) // is white
-{
-
-	setLEDColor(ledRed); // Sæt LED farve til rød, når robotten er på det hvide
-	colorread = 3;
-
-	if (stateR < stateL) // Turning left
+	while (color_cal == true) // Når kalibrering er startet,
 	{
-		setMotorSpeed(motorR, 0);
-		setMotorSpeed(motorL, 10);
-		state = 0;
+		int data;
+		int statebutton;
+		data = SensorValue[colorsens];
+
+		if (SensorValue(touchsens) == 1)
+		{
+			statebutton++;
+			setLEDColor(ledRedFlash);
+			sleep(700);
+		}
+		if (statebutton == 1) // kalibrering af grå farve
+		{
+			displayCenteredTextLine(2, "Calibrating");
+			displayCenteredTextLine(3, "color sensor");
+		}
+
+		displayCenteredTextLine(2, "gray: ", SensorValue[colorsens]);
+		if (statebutton == 2) // kalibrering af grå farve
+		{
+			gray_val = SensorValue[colorsens];
+			setLEDColor(ledGreenFlash);
+			sleep(700);
+			setLEDColor(ledRedFlash);
+			sleep(700);
+		}
+		displayCenteredTextLine(2, "white: ", SensorValue[colorsens]);
+		if (statebutton == 3) // kalibrering af hvid farve
+		{
+			white_val = SensorValue[colorsens];
+			setLEDColor(ledGreenFlash);
+			sleep(700);
+			setLEDColor(ledRedFlash);
+			sleep(700);
+			perfect_line = gray_val + ((white_val - gray_val) / 2);
+			eraseDisplay();
+			color_cal = false;
+		}
 	}
-	else if (stateR > stateL) // turning right
-	{
-		setMotorSpeed(motorR, 10);
-		setMotorSpeed(motorL, 0);
-		state = 0;
-	}
-*/
+}
