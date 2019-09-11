8b8f331e0917823becdd97884cc2465db48d2dfa
peterkrull <31969909+peterkrull@users.noreply.github.com>
2019-09-11 13:29:58 +0200

    Fikset color_calibrate

--- RoboCup main.c
+++ RoboCup main.c
@@ -24,7 +24,7 @@ int curr_task = 0;		  // For at teste specifikke udfordringer, skift dette tal
 int dir = 1;			  // sæt 1 for at køre på venstre side af grå streg, 0 for at køre på højre
 int black_counter;		  // Bruges til at holde styr på antallet af krydsede sorte linjer
 float perfect_line;		  // variabel til at holde information om den kalibrerede linje
-float speed = 25;		  // Robottens hastighed i PID-loopet.
+float speed = 20;		  // Robottens hastighed i PID-loopet.
 
 // Variabel til at holde sensor aflæsning
 int line_sensor_val;
@@ -45,7 +45,11 @@ void Linefollow_PID(bool enable_tracking) // Funktionen der bruges til at følge
 		float Kp = 0.15;
 		float Ki = 0.000001;
 		float Kd = 0.028;
-
+		/*
+		float Kp = 0.1;
+		float Ki = 0.000001;
+		float Kd = 0.02;
+*/
 		// variabler brugt til PID
 		float deltaErr = 0;
 		float turn = 0;
@@ -80,7 +84,6 @@ void Linefollow_PID(bool enable_tracking) // Funktionen der bruges til at følge
 
 		// Udregn sum af fejl : Integral
 		error_sum += errors;
-
 		// Udregn fejl delta : Differentiale
 		deltaErr = errors - prev_error;
 
@@ -115,7 +118,102 @@ void dreje(float turn_degrees)
 	} //while loopet eksisterer for at sikre at robotten er helt drejet før den udfører ny kode
 }
 
-task color_calibrate(); 
+//
+// FUNKTION TIL AT KØRE ET ANTAL CM
+// Indsæt en værdi i move(xx); for at kære ligeud
+
+void move(float CM)
+{
+	float forwardT = (360 / (5.5 * PI)) * CM;
+	resetMotorEncoder(motorL);
+	resetMotorEncoder(motorR);
+	setMotorTarget(motorL, forwardT, 25);
+	setMotorTarget(motorR, forwardT, 25);
+	while (abs(getMotorEncoder(motorL)) < (abs(forwardT) - 4))
+	{ //de minus 4 er en buffer in case motoren ikke rammer target pr�cis
+	} //while loopet eksisterer for at sikre at robotten er k�rt f�rdig f�r den udf�rer ny kode
+}
+
+//
+// FUNKTION TIL AT KALIBRERE GRÅ/HVID FARVE
+// Køres én gang i starten
+
+void color_calibrate() // Funktion til kalibrering af farvesensor
+{
+	bool color_cal;
+	if (SensorValue(calbutton) == 1) // Hvis knappen er trykket ned i mere end 2 sekunder igangsættes farvekalibrering
+	{
+		clearTimer(T2); // reset af timer2
+		while (SensorValue(calbutton) == 1 && color_cal == false)
+		{
+			setLEDColor(ledOrangeFlash);
+			if (time1[T2] > 2000) // hvis timer 2 tælller over 2000 ms
+			{
+				color_cal = true; // igangsættes kalibrering
+				setLEDColor(ledGreenFlash);
+			}
+		}
+	}
+	int calstate = 0;
+	while (color_cal == true) // Når kalibrering er startet,
+	{
+		if (calstate == 0)
+		{
+			displayCenteredBigTextLine(2, "Calibrating");
+			displayCenteredBigTextLine(4, "color sensor");
+			sleep(2000);
+			calstate++;
+		}
+		if (calstate == 1)
+		{
+			displayCenteredBigTextLine(2, "gray: ", "%f", SensorValue[colorsense]);
+			displayCenteredTextLine(4, "");
+			displayCenteredTextLine(5, "press button to calibrate");
+			if (SensorValue[calbutton] == 1)
+			{
+				gray_val = SensorValue[colorsense];
+				eraseDisplay();
+				displayCenteredBigTextLine(2, "Gray calibrated");
+				setLEDColor(ledGreen);
+				sleep(1500);
+				eraseDisplay();
+				setLEDColor(ledOff);
+				calstate++;
+			}
+		}
+		if (calstate == 2)
+		{
+			displayCenteredBigTextLine(2, "white: ", "%f", SensorValue[colorsense]);
+			displayCenteredTextLine(4, "");
+			displayCenteredTextLine(5, "press button to calibrate");
+			if (SensorValue[calbutton] == 1)
+			{
+				white_val = SensorValue[colorsense];
+				eraseDisplay();
+				displayCenteredBigTextLine(2, "White calibrated");
+				setLEDColor(ledGreen);
+				sleep(1500);
+				eraseDisplay();
+				setLEDColor(ledOff);
+				calstate++;
+			}
+		}
+		if (calstate == 3)
+		{
+			perfect_line = gray_val + ((white_val - gray_val) / 2);
+			displayCenteredBigTextLine(2, "Calibration");
+			displayCenteredBigTextLine(4, "done...");
+			sleep(1000);
+			displayCenteredBigTextLine(8, "Place robot");
+			displayCenteredBigTextLine(10, "on the line");
+			sleep(2000);
+			eraseDisplay();
+			curr_task++;
+			color_cal = false;
+		}
+	}
+}
+
 task main()
 {
 	perfect_line = gray_val + ((white_val - gray_val) / 2); // udregner den perfekte linje én gang i starten
@@ -123,19 +221,22 @@ task main()
 	{
 		line_sensor_val = SensorValue(colorsense); // Værdien der læses fra farvesensoren. Skal næsten altid bruges, og er derfor i starten af vores loop.
 
-		startTask(color_calibrate);
-
 		if (curr_task == 0) // Det initielle stadie af robotten. Setup placeres her
 		{
-			Linefollow_PID(true);
+			color_calibrate();
 		}
 
 		if (curr_task == 1)
 		{
 			if (task1 == true) // Betingelser for udførelse af opgave 1
 			{
-				if (black_counter == 1){}
-				if (black_counter == 2){}
+				Linefollow_PID(true);
+				if (black_counter == 1)
+				{
+				}
+				if (black_counter == 2)
+				{
+				}
 				// Indsæt opgave 1 loop her **********************
 			}
 			else
@@ -149,8 +250,12 @@ task main()
 		{
 			if (task2 == true) // Betingelser for udførelse af opgave 2
 			{
-				if (black_counter == 3){}
-				if (black_counter == 4){}
+				if (black_counter == 3)
+				{
+				}
+				if (black_counter == 4)
+				{
+				}
 				// Indsæt opgave 2 loop her **********************
 			}
 			else
@@ -164,8 +269,12 @@ task main()
 		{
 			if (task3 == true) // Betingelser for udførelse af opgave 3
 			{
-				if (black_counter == 5){}
-				if (black_counter == 6){}
+				if (black_counter == 5)
+				{
+				}
+				if (black_counter == 6)
+				{
+				}
 				// Indsæt opgave 3 loop her **********************
 			}
 			else
@@ -179,24 +288,25 @@ task main()
 		{
 			if (task4 == true)
 			{
-				if (black_counter == 7){
-				float distanceR = getMotorEncoder(motorR);
-				float distanceL = getMotorEncoder(motorL);
-				float distance = (distanceR+distanceL)/2;
-				Linefollow_PID(false);
-				dreje(-45);
-				resetMotorEncoder(motorL);
-				resetMotorEncoder(motorR);
-	 			while(distance < 204.1)
-				 {
-					 distanceR = getMotorEncoder(motorR) ;
-					 distanceR = getMotorEncoder(motorL);
-					 distance = (distanceR+distanceL)/2;
-					 motor[motorR]=10; //kører med farten 10
-					 motor[motorR]=10; //kører med farten 10	 
-				 }
-				dreje(+45);
-				Linefollow_PID(true);
+				if (black_counter == 7)
+				{
+					float distanceR = getMotorEncoder(motorR);
+					float distanceL = getMotorEncoder(motorL);
+					float distance = (distanceR + distanceL) / 2;
+					Linefollow_PID(false);
+					dreje(-45);
+					resetMotorEncoder(motorL);
+					resetMotorEncoder(motorR);
+					while (distance < 204.1)
+					{
+						distanceR = getMotorEncoder(motorR);
+						distanceR = getMotorEncoder(motorL);
+						distance = (distanceR + distanceL) / 2;
+						motor[motorR] = 10; //kører med farten 10
+						motor[motorR] = 10; //kører med farten 10
+					}
+					dreje(+45);
+					Linefollow_PID(true);
 				}
 			}
 			else
@@ -223,18 +333,22 @@ task main()
 			if (task6 == true && curr_task == 6 || task8 == true && curr_task == 8) // Betingelser for udførelse af opgave 6 og 8
 			{
 				// Indsæt opgave 6 og 8 loop her **********************
-				Linefollow_PID(false);
-				dreje(80);
-				delay(200);
-				while (line_sensor_val > perfect_line)
+				if (black_counter == 10 || black_counter == 12)
 				{
-					setMotorSpeed(motorR, 12);
-					setMotorSpeed(motorL, 10);
+					move(4);
+					Linefollow_PID(false);
+					dreje(80);
+					delay(200);
+					while (line_sensor_val > perfect_line)
+					{
+						setMotorSpeed(motorR, 12);
+						setMotorSpeed(motorL, 10);
+					}
+					delay(200);
+					dreje(-80);
+					Linefollow_PID(true);
+					curr_task++;
 				}
-				delay(200);
-				dreje(-80);
-				Linefollow_PID(true);
-				curr_task++;
 			}
 			else
 			{
@@ -271,85 +385,6 @@ task main()
 	{
 		// Afspil lyd
 		// Kør rundt i cirkler
-		// Åben og luk grappen
-	}
-}
-
-/*
-task grap_homing()
-{
-}*/
-
-task color_calibrate() // Funktion til kalibrering af farvesensor
-{
-	bool color_cal;
-	if (SensorValue(calbutton) == 1) // Hvis knappen er trykket ned i mere end 2 sekunder igangsættes farvekalibrering
-	{
-		clearTimer(T2); // reset af timer2
-		while (SensorValue(calbutton) == 1)
-		{
-			setLEDColor(ledOrangeFlash);
-			if (time1[T2] > 2000) // hvis timer 2 tælller over 2000 ms
-			{
-				color_cal = true; // igangsættes kalibrering
-				setLEDColor(ledGreenFlash);
-				sleep(500);
-			}
-		}
-	}
-
-	while (color_cal == true) // Når kalibrering er startet,
-	{
-		int calstate = 0;
-
-		if (calstate == 0)
-		{
-			displayCenteredTextLine(2, "Calibrating");
-			displayCenteredTextLine(3, "color sensor");
-			sleep(1000);
-			calstate++;
-		}
-
-		if (calstate == 1)
-		{
-			displayCenteredTextLine(2, "gray: ", SensorValue[colorsense]);
-			displayCenteredTextLine(3, "press button to calibrate");
-			if (SensorValue[calbutton] == 1)
-			{
-				gray_val = SensorValue[colorsense];
-				eraseDisplay();
-				displayCenteredTextLine(2, "Gray calibrated");
-				setLEDColor(ledGreen);
-				sleep(500);
-				eraseDisplay();
-				setLEDColor(ledOff);
-				calstate++;
-			}
-		}
-
-		if (calstate == 2)
-		{
-			displayCenteredTextLine(2, "white: ", SensorValue[colorsense]);
-			displayCenteredTextLine(3, "press button to calibrate");
-			if (SensorValue[calbutton] == 1)
-			{
-				white_val = SensorValue[colorsense];
-				eraseDisplay();
-				displayCenteredTextLine(2, "White calibrated");
-				setLEDColor(ledGreen);
-				sleep(500);
-				eraseDisplay();
-				setLEDColor(ledOff);
-				calstate++;
-			}
-		}
-		if (calstate == 3)
-		{
-			perfect_line = gray_val + ((white_val - gray_val) / 2);
-			displayCenteredTextLine(2, "Calibration done");
-			sleep(500);
-			eraseDisplay();
-			color_cal = false;
-		}
+		// �?ben og luk grappen
 	}
 }
